801f8cce3489f418f5cd1e0b6aff2bc3b559c8a8
Elena Sidina <lenkasid@tut.by>
2023-02-05 22:28:24 +0300

    add Dog constructors

--- src/main/java/ru/clevertec/gitflow/entity/Dog.java
+++ src/main/java/ru/clevertec/gitflow/entity/Dog.java
@@ -2,4 +2,17 @@ package ru.clevertec.gitflow.entity;
 
 public class Dog extends Animal {
     private boolean isTrained;
+
+    public Dog() {
+    }
+
+    public Dog(String name, boolean isTrained) {
+        super(name);
+        this.isTrained = isTrained;
+    }
+
+    public Dog(Long id, String name, boolean isTrained) {
+        super(id, name);
+        this.isTrained = isTrained;
+    }
 }
