e00d67a97e837a7f09e8ccdb3f46db191cca6c32
Elena Sidina <lenkasid@tut.by>
2023-02-05 21:38:53 +0300

    add Animal setters

--- src/main/java/ru/clevertec/gitflow/entity/Animal.java
+++ src/main/java/ru/clevertec/gitflow/entity/Animal.java
@@ -23,4 +23,12 @@ public class Animal {
     public String getName() {
         return name;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
